a557d8791b11c3795b6d523c929bbedcf9a0bc9b
acsBenceTamas <acs.bence.tamas@gmail.com>
2019-01-29 18:16:21 +0100

    Residents modal, url tracks page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,9 +8,13 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/list-planets')
+@app.route('/list-planets/')
 def list_planets():
-    return render_template('list_planets.html')
+    page = 1
+    if request.args:
+        if request.args["page"]:
+            page = int(request.args["page"])
+    return render_template('list_planets.html', page=page)
 
 
 if __name__ == '__main__':
--- static/js/planets.js
+++ static/js/planets.js
@@ -1,5 +1,9 @@
-function listPlanets( apiTarget='https://swapi.co/api/planets')
+function listPlanets( apiTarget )
 {
+    const apiPage = new URL(apiTarget).searchParams.get("page");
+    const url = new URL(window.location.href);
+    url.searchParams.set("page", apiPage);
+    history.replaceState(history.state, `page ${apiPage}`, url);
     startLoading();
     fetch(apiTarget)
     .then((response) => response.json())
@@ -8,29 +12,40 @@ function listPlanets( apiTarget='https://swapi.co/api/planets')
     })
 }
 
-function listResidents( apiTargetList )
+function listResidents( apiTargetList, planetName )
 {
     startLoading();
-    let fetchId = 0;
     let residents = [];
-    fetch(apiTarget)
-    .then((response) => response.json())
-    .then((data) => {
-        residents.push(data);
-    });
-    populateResidentList( residents );
+    let promises = [];
+    for (let apiTarget of apiTargetList) {
+        let promise = fetch(apiTarget)
+        .then((response) => response.json())
+        .then((data) => {
+            residents.push(data);
+        });
+        promises.push(promise);
+    }
+    Promise.all(promises).then(x => populateResidentList( residents, planetName ))
 }
 
-function populateResidentList( data )
+function populateResidentList( data, planetName )
 {
     clearResidentList();
     for (let resident of data) {
         let row = document.createElement('div');
         row.classList.add('row');
-        row.classList.add('planet-row');
+        row.classList.add('resident-row');
         row.appendChild(constructTextColumn(resident.name, 2));
-        row.appendChild(constructTextColumn(`${Number(resident.height)/100} m`));
-        row.appendChild(constructTextColumn(`${resident.mass} kg`));
+        row.appendChild(constructTextColumn(
+            isNaN(resident.height) ?
+                "unknown":
+                `${Number(resident.height)/100} m`
+        ));
+        row.appendChild(constructTextColumn(
+            isNaN(resident.mass) ?
+                "unknown":
+                `${resident.mass} kg`
+        ));
         row.appendChild(constructTextColumn(resident.skin_color));
         row.appendChild(constructTextColumn(resident.hair_color));
         row.appendChild(constructTextColumn(resident.eye_color));
@@ -40,20 +55,19 @@ function populateResidentList( data )
                 resident.gender === "female" ? "♀" :
                     resident.gender
         ));
+        document.getElementById('resident-list').appendChild(row);
     }
+    document.getElementById('residentsModalLabel').innerText = `Residents of ${planetName}`;
     $('#residentsModal').modal();
     stopLoading();
 }
 
 function populatePlanetList( data )
 {
-    console.log("populating resident list");
     clearPlanetList();
     let planetList = document.getElementById('planet-list');
     planetList.dataset.next = JSON.stringify(data.next);
     planetList.dataset.previous = JSON.stringify(data.previous);
-    document.getElementById('btn-next').disabled = data.next === null;
-    document.getElementById('btn-previous').disabled = data.previous === null;
     for (let planet of data.results) {
         let row = document.createElement('div');
         row.classList.add('row');
@@ -72,15 +86,20 @@ function populatePlanetList( data )
             planet.population.includes("unknown") ? "unknown" :
                 `${Number(planet.population).toLocaleString()} people`,
             2));
-        row.appendChild(constructButtonColumn(
-            `${planet.residents.length} resident(s)`,
-            ["btn-residents", "btn", "btn-sm", "btn-secondary"],
-            {residents: planet.residents},
-            2
-            ));
+        if (planet.residents.length > 0) {
+            row.appendChild(constructButtonColumn(
+                `${planet.residents.length} resident(s)`,
+                ["btn-residents", "btn", "btn-sm", "btn-secondary"],
+                {residents: planet.residents, name: planet.name},
+                2
+                ));
+        } else {
+            row.appendChild(constructTextColumn("No known residents", 2));
+        }
         row.appendChild(constructButtonColumn(
             `Vote`,
-            ["btn-vote-planet"]
+            ["btn-vote-planet", "btn", "btn-sm", "btn-secondary"],
+            {planetUrl: getIdFromURL(planet.url)}
             ));
         document.getElementById('planet-list').appendChild(row);
     }
@@ -114,12 +133,13 @@ function constructTextColumn(content, relativeWidth=1, collapseThreshold='xl')
     return column
 }
 
-function constructButtonColumn(title, classList = [], dataset={}, relativeWidth=1, collapseThreshold='xl')
+function constructButtonColumn(title, classList = [], dataset={}, relativeWidth=1, collapseThreshold='xl', disabled= false)
 {
     let column = document.createElement('div');
     column.classList.add(`col-${collapseThreshold}-${relativeWidth}`);
     let button = document.createElement('button');
     button.innerText = title;
+    button.disabled = disabled;
     for (let index in dataset) button.dataset[index] = JSON.stringify(dataset[index]);
     for (let classItem of classList) button.classList.add(classItem);
     column.appendChild(button);
@@ -142,6 +162,9 @@ function stopLoading() {
     if (loadingIcon) {
         loadingIcon.remove();
     }
+    let planetList = document.getElementById('planet-list');
+    document.getElementById('btn-next').disabled = JSON.parse(planetList.dataset.next) === null;
+    document.getElementById('btn-previous').disabled = JSON.parse(planetList.dataset.previous) === null;
 }
 
 function planetListClick(event) {
@@ -151,16 +174,22 @@ function planetListClick(event) {
         } else if (event.target.id.toString() === 'btn-previous') {
             listPlanets(JSON.parse(event.currentTarget.dataset.previous));
         } else if (event.target.classList.contains('btn-residents')) {
-            listResidents( JSON.parse(event.target.dataset.residents) );
+            listResidents( JSON.parse(event.target.dataset.residents), JSON.parse(event.target.dataset.name) );
             document.getElementById('residentsModalLabel').innerText = "";
         }
     }
 }
 
+function getIdFromURL( url ) {
+    let parts = url.split("/");
+    return parts[parts.length-2];
+}
+
 function main()
 {
     document.getElementById('planet-list').addEventListener('click', planetListClick);
-    listPlanets();
+    const dataContainer = document.getElementById("server-data-container");
+    listPlanets(`https://swapi.co/api/planets/?page=${dataContainer.dataset.page ? dataContainer.dataset.page: 1}`);
 }
 
 main();
